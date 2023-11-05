9fcaeaa7f3ac6a628e6916d43218dcb033a544f4
Moustafa Elsayed <moelsayed949@gmail.com>
2023-11-05 20:16:55 +0200

    add product to cart after select

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -10,19 +10,13 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Container from "@mui/material/Container";
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+import Button from "@mui/material/Button";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
 const Cart = () => {
   const product = useSelector((state) => state.product);
+  const cart = useSelector((state) => state.cart);
+
   console.log(product);
   const dispatch = useDispatch();
   useEffect(() => {
@@ -35,26 +29,31 @@ const Cart = () => {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>ID</TableCell>
+              <TableCell align="left">Title</TableCell>
+              <TableCell align="left">Imgage</TableCell>
+              <TableCell align="left">Price</TableCell>
+              <TableCell align="left">Actions</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {cart.map((product) => (
               <TableRow
-                key={row.name}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                key={product.id}
               >
                 <TableCell component="th" scope="row">
-                  {row.name}
+                  {product.id}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="left">{product.title}</TableCell>
+                <TableCell align="left"><img width="30px" src={product.image}  alt=""/></TableCell>
+                <TableCell align="left">{product.price}</TableCell>
+
+                <TableCell align="left">
+                  <Button variant="text" color="primary">
+                    <DeleteForeverIcon />
+                  </Button>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
