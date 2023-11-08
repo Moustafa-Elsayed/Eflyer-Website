1d1117aa147f772cd9f570770cf9f980797020ae
Moustafa Elsayed <moelsayed949@gmail.com>
2023-11-09 00:40:55 +0200

    create footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./footer.css"
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <h1>© 2020 All Rights Reserved. Design by Free html Templates</h1>
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ProductList from "../product/ProductList";
 import Container from "@mui/material/Container";
 import Textswiper from "../../components/header/Textswiper";
+import Footer from "../../components/footer/Footer";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Container maxWidth="lg">
         <ProductList />
       </Container>
+      <Footer />
     </>
   );
 };
